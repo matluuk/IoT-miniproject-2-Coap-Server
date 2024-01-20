fc23fcccf98ccfdd32552fd95620ad4845f578a7
Matti Luukkonen <matti.luukkonen12@gmail.com>
2024-01-20 21:35:05 +0200

    Add /api/config resource

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,8 @@ import sys
 
 from flask import Flask, jsonify
 from flask_cors import CORS
-from aiocoap import Context, Message, GET
+from flask import request
+from aiocoap import Context, Message, GET, PUT
 import asyncio
 
 DEFAULT_IP_ADRESS = "10.166.0.2"
@@ -61,11 +62,11 @@ def get_data():
         # Create a new CoAP client
         protocol = await Context.create_client_context()
 
-        # Create a new GET request
-        request = Message(code=GET, uri=coap_server_url)
+        # Create a new GET coap_request
+        coap_request = Message(code=GET, uri=coap_server_url)
 
-        # Send the request
-        response = await protocol.request(request).response
+        # Send the coap_request
+        response = await protocol.request(coap_request).response
 
         # Parse the response as JSON
         data = json.loads(response.payload.decode())
@@ -78,6 +79,56 @@ def get_data():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     return loop.run_until_complete(main())
+
+@app.route('/api/config', methods=['GET', 'PUT'])
+def config():
+    async def handle_get():
+        # Replace with your CoAP server's address
+        coap_server_url = f'coap://{server_ip_address}:{server_port}/data'
+        
+        # Create a new CoAP client
+        protocol = await Context.create_client_context()
+
+        # Create a new GET coap_request
+        coap_request = Message(code=GET, uri=coap_server_url)
+
+        # Send the coap_request
+        response = await protocol.request(coap_request).response
+
+        # Parse the response as JSON
+        data = json.loads(response.payload.decode())
+
+        print(data)
+
+        # Return the data as JSON
+        return jsonify(data)
+    
+    async def handle_put():
+        # Replace with your CoAP server's address
+        coap_server_url = f'coap://{server_ip_address}:{server_port}/data'
+        
+        # Create a new CoAP client
+        protocol = await Context.create_client_context()
+
+        # Get the JSON data from the request
+        data = request.get_json()
+
+        # Create a new PUT request
+        coap_request = Message(code=PUT, uri=coap_server_url, payload=json.dumps(data))
+
+        # Send the request
+        response = await protocol.request(request).response
+
+        # Return the response status code
+        return str(response.code)
+
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    if request.method == 'GET':
+        return loop.run_until_complete(handle_get())
+    elif request.method == 'PUT':
+        return loop.run_until_complete(handle_put())
 
 if __name__ == '__main__':
     parse_args()
