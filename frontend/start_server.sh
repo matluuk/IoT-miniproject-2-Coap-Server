72f94f8e142d328c02df6de0c67a1436b068b725
Matti Luukkonen <matti.luukkonen12@gmail.com>
2024-01-30 16:05:39 +0200

    Add ip address + port variables to index.html and start_server.sh

--- frontend/start_server.sh
+++ frontend/start_server.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+app_ip="127.0.0.1"
+app_port="5000"
+
+CoAp_server_ip="127.0.0.1"
+CoAp_server_port="5683"
+
+http_server_ip="127.0.0.1"
+http_server_port="8000"
 
 start_server()
 {
         rm $dir/nohup_server.log
 
-        nohup python3 server.py $@ > nohup_server.log 2>&1 &
+        nohup python3 server.py --ip=$http_server_ip --port=$http_server_port > nohup_server.log 2>&1 &
         echo $! > save_pid_server.txt
         echo "server started and pid saved to file"
 }
@@ -14,7 +22,7 @@ start_app()
 {
         rm $dir/nohup_app.log
 
-        nohup python3 app.py $@ > nohup_app.log 2>&1 &
+        nohup python3 app.py --ip=$app_ip --port=$app_port --server_ip=$CoAp_server_ip --server_port=$CoAp_server_port > nohup_app.log 2>&1 &
         echo $! > save_pid_app.txt
         echo "App started and pid saved to file"
 }
